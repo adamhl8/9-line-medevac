3bfd5e85ab4534aa5245d14e522c989a4d0bab47
Marcus Scott <marcus.a.scott12.mil@swf.army.mil>
2022-11-09 10:54:46 -0600

    add basic home page with conditional rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import { MantineProvider } from "@mantine/core"
-
+import { AppShell, Header, MantineProvider } from "@mantine/core"
+import MainPage from "./components/MainPage"
 const App = () => {
-
   return (
-    <MantineProvider theme={{ colorScheme: "light" }} withGlobalStyles withNormalizeCSS>
+    <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles withNormalizeCSS>
+      <AppShell
+        padding="md"
+        header={
+          <Header height={60} p="xs">
+            test test
+          </Header>
+        }
+        styles={(theme) => ({
+          main: { backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+      >
+        <MainPage />
+      </AppShell>
     </MantineProvider>
   )
 }
