a16c09f9ec45aa806ca44b9dc9cbd887ff8bdaf9
Christopher Reif <35343166+creif94@users.noreply.github.com>
2022-11-10 14:04:22 -0600

    add validation to the first 5 lines, defaulted Special Equipment to None

--- client/src/components/Requestor/Requestor.tsx
+++ client/src/components/Requestor/Requestor.tsx
@@ -1,14 +1,34 @@
-import { Grid } from '@mantine/core';
+import { Grid, Box, Stack } from '@mantine/core';
 import Fourm from './Fourm'
+import {useState} from "react";
 
 const Requestor = () =>{
+    const [submitted, setSubmitted] = useState(false)
 
     return (<>
-        <Grid>
-            <Grid.Col span={4}></Grid.Col>
-            <Grid.Col span={5}><Fourm /></Grid.Col>
-            <Grid.Col span={3}></Grid.Col>
-        </Grid>
+        <Stack>
+            <Box
+                sx={(theme) => ({
+                    backgroundColor: submitted ? 'green' : 'red',
+                    textAlign: 'center',
+                    padding: theme.spacing.xl,
+                    borderRadius: theme.radius.md,
+                    // cursor: 'pointer',
+
+                    // '&:hover': {
+                    //     backgroundColor:
+                    //         theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+                    // },
+                })}
+            >
+                <b>YOU HAVE NOT SUBMITTED YOUR 9-LINE</b>
+            </Box>
+            <Grid>
+                <Grid.Col span={2}></Grid.Col>
+                <Grid.Col span={12}><Fourm /></Grid.Col>
+                <Grid.Col span={2}></Grid.Col>
+            </Grid>
+        </Stack>
         </>)
 }
 
