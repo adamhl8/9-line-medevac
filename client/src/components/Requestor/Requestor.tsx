f62f431abd03e62c1295d0a32d5ab20e3815b59f
Gear-Smith <rehuff83@gmail.com>
2022-11-17 11:20:35 -0600

    added current request modal, views current request in state

--- client/src/components/Requestor/Requestor.tsx
+++ client/src/components/Requestor/Requestor.tsx
@@ -1,6 +1,30 @@
-import { Box, Button, Grid, Stack } from "@mantine/core"
+import { ActionIcon, Box, Button, Grid, Group, Stack } from "@mantine/core"
+import { IconListDetails } from "@tabler/icons"
 import { useState } from "react"
+import { z } from "zod"
 import Form from "./Form"
+
+export const RequestBody = z.object({
+  status: z.string(),
+  location: z.string(),
+  callSign: z.string(),
+  frequency: z.string(),
+  byAmbulatory: z.number(),
+  byLitter: z.number(),
+  specialEquipment: z.string(),
+  byUrgent: z.number(),
+  byPriority: z.number(),
+  byRoutine: z.number(),
+  security: z.string(),
+  marking: z.string(),
+  usMil: z.number(),
+  usCiv: z.number(),
+  nonUSMil: z.number(),
+  nonUSCiv: z.number(),
+  nbc: z.string(),
+})
+
+export type TRequestBody = z.infer<typeof RequestBody>
 
 interface settingView {
   setView: React.Dispatch<React.SetStateAction<string>>
@@ -8,6 +32,8 @@ interface settingView {
 
 const Requestor = (props: settingView) => {
   const [submitted, setSubmitted] = useState(false)
+  const [opened, setOpened] = useState(false)
+  const [request, setRequest] = useState<TRequestBody>()
 
   return (
     <>
@@ -24,7 +50,7 @@ const Requestor = (props: settingView) => {
         })}
       >
         <Grid justify="center">
-          <Grid.Col span={4}>
+          <Grid.Col span="auto">
             <Button.Group>
               <Button
                 variant="light"
@@ -55,14 +81,37 @@ const Requestor = (props: settingView) => {
               </Button>
             </Button.Group>
           </Grid.Col>
-          <Grid.Col span={4}>
+          <Grid.Col span="auto">
             <b>{submitted ? "9-LINE HAS BEEN SUBMITTED" : "9-LINE HAS NOT BEEN SUBMITTED"} </b>
           </Grid.Col>
-          <Grid.Col span={4}></Grid.Col>
+          <Grid.Col span={2} offset={2}>
+            <Group>
+              <b>Current Request: </b>
+
+              <ActionIcon
+                variant="outline"
+                aria-label="Current"
+                onClick={() => {
+                  console.log(request)
+                  setRequest(request)
+                  setOpened(true)
+                }}
+              >
+                <IconListDetails size={18} />
+              </ActionIcon>
+            </Group>
+          </Grid.Col>
         </Grid>
       </Box>
       <Stack align="center">
-        <Form setSubmitted={setSubmitted} submitted={submitted} />
+        <Form
+          setSubmitted={setSubmitted}
+          submitted={submitted}
+          opened={opened}
+          setOpened={setOpened}
+          request={request}
+          setRequest={setRequest}
+        />
       </Stack>
     </>
   )
