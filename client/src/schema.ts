dfe6233f9ffc7ea1b8abf496a8be59ef0579c897
Adam Tomanovich <adamtomanovich@comcast.net>
2022-11-21 13:53:32 -0600

    wip

--- client/src/schema.ts
+++ client/src/schema.ts
@@ -1,28 +1,30 @@
 import { z } from "zod"
 
-export const RequestById = z.object({
-  id: z.number().default(0),
-  status: z.string().min(1),
-  location: z.string().default("Austin"),
-  callSign: z.string().default("Nik"),
-  frequency: z.number().default(0),
-  byAmbulatory: z.number().default(0),
-  byLitter: z.number().default(0),
-  specialEquipment: z.string().default("jungle PENETRATOR"),
-  byUrgent: z.number().default(0),
-  byPriority: z.number().default(0),
-  byRoutine: z.number().default(0),
-  security: z.string().default("No Enemy in the Area").nullable(),
-  marking: z.string().default("None").nullable(),
-  usMil: z.number().default(0).nullable(),
-  usCiv: z.number().default(0).nullable(),
-  nonUSMil: z.number().default(0).nullable(),
-  nonUSCiv: z.number().default(0).nullable(),
-  nbc: z.string().default("None").nullable(),
-  responderID: z.number().default(0).nullable(),
-  dispatcherID: z.number().default(0).nullable(),
-})
+const defaultString = (value: unknown) => value ?? ""
+const defaultNumber = (value: unknown) => value ?? 0
 
+export const RequestById = z.object({
+  id: z.number().transform(defaultNumber),
+  status: z.string().transform(defaultString),
+  location: z.string().transform(defaultString),
+  callSign: z.string().transform(defaultString),
+  frequency: z.number().transform(defaultNumber),
+  byAmbulatory: z.number().transform(defaultNumber),
+  byLitter: z.number().transform(defaultNumber),
+  specialEquipment: z.string().transform(defaultString),
+  byUrgent: z.number().transform(defaultNumber),
+  byPriority: z.number().transform(defaultNumber),
+  byRoutine: z.number().transform(defaultNumber),
+  security: z.string().transform(defaultString),
+  marking: z.string().transform(defaultString),
+  usMil: z.number().transform(defaultNumber),
+  usCiv: z.number().transform(defaultNumber),
+  nonUSMil: z.number().transform(defaultNumber),
+  nonUSCiv: z.number().transform(defaultNumber),
+  nbc: z.string().transform(defaultString).nullish(),
+  responderID: z.number().transform(defaultNumber),
+  dispatcherID: z.number().transform(defaultNumber),
+})
 export type TRequestById = z.infer<typeof RequestById>
 
 export const RequestData = RequestById.array()
