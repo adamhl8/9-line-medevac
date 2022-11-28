ba41e520e9ae1ffa0068b640a9ce0da2a44d8f75
Adam Tomanovich <adamtomanovich@comcast.net>
2022-11-28 15:57:44 -0600

    added filter to responder and dispatcher, need to fix number to string on Precedence

--- client/src/components/SharedTable.tsx
+++ client/src/components/SharedTable.tsx
@@ -1,47 +1,82 @@
-import { Pagination, Stack, Table } from "@mantine/core"
-import { useState } from "react"
-import { RequestData, TRequestData } from "../schema.js"
+import {Pagination, Stack, Table, TextInput} from "@mantine/core"
+import {ChangeEvent, useState} from "react"
+import {RequestData, TRequestData} from "../schema.js"
 import store from "../store.js"
 import SharedModal from "./SharedModal"
 import TableRow from "./TableRow"
 
 function SharedTable() {
-  const pages = store((state) => state.pages)
-  const tableHeaders = store((state) => state.tableHeaders)
-  const page1 = RequestData.parse((pages && pages[0]) || pages) // if pages exists, pass in first page array, otherwise pages must be empty array
-  const [pageNumber, setPageNumber] = useState(1)
-  const [page, setPage] = useState<TRequestData>(page1)
+    const pages = store((state) => state.pages)
+    const tableHeaders = store((state) => state.tableHeaders)
+    const page1 = RequestData.parse((pages && pages[0]) || pages) // if pages exists, pass in first page array, otherwise pages must be empty array
+    const [pageNumber, setPageNumber] = useState(1)
+    const [page, setPage] = useState<TRequestData>(page1)
+    const [searchData, setSearchData] = store((state) => [state.searchData, state.setSearchData])
 
-  if (!pages) return <></>
 
-  const rows = page.map((request, i) => {
-    return <TableRow key={i} request={request} />
-  })
+    if (!pages) return <></>
 
-  const handleSetPage = (pageNumber: number) => {
-    setPageNumber(pageNumber)
-    setPage(RequestData.parse(pages[pageNumber - 1]))
-  }
-//finding how to sort
+    const rows = page
+        .filter((filterData)=>{
+           if (filterData.status.toLowerCase().toString().includes(searchData)){
+               return filterData
+           } else if (filterData.location.toLowerCase().toString().includes(searchData)){
+               return filterData
+           } else if (filterData.callSign.toLowerCase().toString().includes(searchData)){
+               return filterData
+           } else if (filterData.frequency.toString().includes(searchData)){
+               return filterData
+           } else if (filterData.security.toLowerCase().toString().includes(searchData)){
+               return filterData
+           } else if (filterData.marking.toLowerCase().toString().includes(searchData)){
+               return filterData
+           } else if (filterData.responderID.toString().includes(searchData)){
+               return filterData
+           } else if (filterData.specialEquipment.toLowerCase().toString().includes(searchData)){
+               return filterData
+           }   else if (filterData.byUrgent.toString().startsWith("Urgent",0)) {
+               return filterData
+           }
+        })
+        .map((request, i) => {
+        return <TableRow key={i} request={request}/>
+    })
 
-  return (
-    <>
-      <SharedModal />
-      <Stack>
-        <Table maw="75vw" striped highlightOnHover captionSide="bottom" fontSize="md">
-          <thead>
-            <tr>
-              {tableHeaders.map((header) => (
-                <th key={header}>{header.replace(/([a-z](?=[A-Z]))/g, "$1 ").toUpperCase()}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </Table>
-        <Pagination page={pageNumber} onChange={handleSetPage} total={pages.length} />
-      </Stack>
-    </>
-  )
+    const handleSetPage = (pageNumber: number) => {
+        setPageNumber(pageNumber)
+        setPage(RequestData.parse(pages[pageNumber - 1]))
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        setSearchData(e.target.value)
+    }
+
+    return (
+        <>
+            <SharedModal/>
+            <TextInput
+                onChange={handleChange}
+                value={searchData}
+
+            >
+            </TextInput>
+            <Stack>
+                <Table maw="75vw" striped highlightOnHover captionSide="bottom" fontSize="md">
+                    <thead>
+                    <tr>
+                        {tableHeaders.map((header) => (
+                            <th key={header}>{header.replace(/([a-z](?=[A-Z]))/g, "$1 ").toUpperCase()}</th>
+                        ))}
+                    </tr>
+                    </thead>
+                    <tbody>{rows}</tbody>
+                </Table>
+                <Pagination page={pageNumber} onChange={handleSetPage} total={pages.length}/>
+            </Stack>
+        </>
+    )
+
 }
 
 export default SharedTable
