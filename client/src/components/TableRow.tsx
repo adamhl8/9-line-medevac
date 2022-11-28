8a061db5471533bcf8d18dc3a2d6304488dd00fc
Gear-Smith <rehuff83@gmail.com>
2022-11-28 16:06:46 -0600

    add a form to submit responder ID to the server

--- client/src/components/TableRow.tsx
+++ client/src/components/TableRow.tsx
@@ -25,7 +25,7 @@ const TableRow = ({ request }: TableRowProps) => {
     const lowerStatus = request.status.toLowerCase()
     const pending = lowerStatus === "pending" && request.responderID !== 0 ? "orange" : "red"
     const statusColor = lowerStatus === "complete" ? "green" : pending
-    
+
     if (lowerStatus === "role 2" || lowerStatus === "role rwo") return "cyan"
     return statusColor
   }
@@ -33,7 +33,7 @@ const TableRow = ({ request }: TableRowProps) => {
   return (
     <>
       <tr>
-        <td style={{color: colorizeStatus()}}>{request.status.toUpperCase()}</td>
+        <td style={{ color: colorizeStatus() }}>{request.status.toUpperCase()}</td>
         <td>{request.location}</td>
         <td>{request.callSign}</td>
         <td>{precedence()}</td>
--- server/src/main/java/spring/ResponderRepo.java
+++ server/src/main/java/spring/ResponderRepo.java
@@ -3,5 +3,5 @@ package spring;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ResponderRepo extends CrudRepository<Responder, Long>{
-    
+    public Responder findByResponderId(int responderId);
 }
